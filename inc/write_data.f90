c42cb59b02faab04473bd61bfa1be65e62a78297
Eric Mascot <slek120@gmail.com>
2015-10-29 14:37:01 -0500

    change length of filename

--- inc/write_data.f90
+++ inc/write_data.f90
@@ -22,7 +22,7 @@ real(dp)    :: Pi, t, mu, x0, epsf, V, Uc, Uf, qx, qy
 complex(dp) :: omega, Gkinv(16), Gkqinv(16)
 character(len=8) :: date
 character(len=4) :: time
-character(len=16):: filename
+character(len=12):: filename
 character(len=64):: title
 
 ! Local variables
